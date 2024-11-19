c2740beb3b83dddcf1fa143da85d211dff525fd4
mokaif10 <vhorakaif123@gmail.com>
2024-11-19 18:59:56 +0530

    Add files via upload

---
+++ InvalidSalaryException.java
@@ -0,0 +1,13 @@
+// package in.ac.adit.pwj.miniproject.employees.models.src.in.ac.adit.pwj.miniproject.employees.models.src.in.ac.adit.pwj.miniproject.employees.models.src.in.ac.adit.pwj.miniproject.employees.management.src.in.ac.adit.pwj.miniproject.employees.src.in.ac.adit.pwj.miniproject.employees.exceptions;
+
+// public class InvalidSalaryException {
+    
+// }
+
+package in.ac.adit.pwj.miniproject.employees.exceptions;
+
+public class InvalidSalaryException extends Exception {
+    public InvalidSalaryException(String message) {
+        super(message);
+    }
+}
